6925fcc644d6dae4a309db199caa1117106c5266
Vladislav <lapuzenko18@gmail.com>
2025-10-20 21:49:39 +0300

    разбиение на несколько ролей

---
+++ roles/postgre_restore/templates/restore_db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+LATEST_BACKUP=$(ls -t {{ backup_dir }}/full/*.sql | head -n 1)
+if [ -z "$LATEST_BACKUP" ]; then
+  echo "No backup found!"
+  exit 1
+fi
+echo "Restoring from $LATEST_BACKUP..."
+gunzip -c "$LATEST_BACKUP" | psql -U postgres
+echo "Restore complete."
