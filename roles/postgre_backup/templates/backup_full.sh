7f184e88727551a8376a8c1afe42ec1feb26d36e
Vladislav <lapuzenko18@gmail.com>
2025-11-19 20:48:33 +0300

    final v 1.0

--- roles/postgre_backup/templates/backup_full.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-BACKUP_DIR="{{ backup_dir }}/full"
-mkdir -p "$BACKUP_DIR"
-TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-pg_dumpall -U postgres | gzip > "$BACKUP_DIR/full_$TIMESTAMP.sql.gz"
-find "$BACKUP_DIR" -type f -mtime +7 -delete
